0bda7a4984f4185cb6c695bdeb003c26eb35930a
Ragnar Valdre <RValdre@users.noreply.github.com>
2021-09-21 11:30:26 +0300

    Neljanda tunni lõpp

--- add_film.php
+++ add_film.php
@@ -3,8 +3,15 @@
 	require_once("fnc_film.php");
 	//echo $server_host;
 	$author_name = "Ragnar Valdre";
-	$film_html = null;
-	$film_html = read_all_films();
+	$film_store_notice = null;
+	//kas klikiti submit nuppu
+	if(isset($_POST['film_submit'])){
+		if(!empty($_POST['title_input']) and !empty($_POST['genre_input']) and !empty($_POST['studio_input']) and !empty($_POST['director_input'])){
+			$film_store_notice = store_film($_POST['title_input'], $_POST['year_input'], $_POST['duration_input'], $_POST['genre_input'], $_POST['studio_input'], $_POST['director_input']);
+		} else {
+			$film_store_notice = 'Osa andmeid on puudu !';
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -32,6 +39,10 @@
 	ul {
         color: rgb(209, 209, 209);
     }
+	
+	label {
+		color: rgb(209, 209, 209);
+	}
 	
 	li {
         color: rgb(209, 209, 209);
@@ -81,7 +92,28 @@
     <hr>
     <br>
 	<h2 style="text-align: center;">Eesti Filmid</h2>
-	<?php echo $film_html; ?>
+	<form method='POST'>
+		<label for='title_input'>Filmi pealkiri: </label>
+		<input type='text' name='title_input' id='title_input' placeholder='pealkiri'>
+		<br>
+		<label for='year_input'>Valmimis aasta: </label>
+		<input type='number' name='year_input' id='year_input' value='<?php echo date('Y'); ?>' min='1912'>
+		<br>
+		<label for='duration_input'>Kestus minutites: </label>
+		<input type='number' name='duration_input' id='duration_input' value='60' min='1'>
+		<br>
+		<label for='genre_input'>Filmi žanr: </label>
+		<input type='text' name='genre_input' id='genre_input' placeholder='žanr'>
+		<br>
+		<label for='studio_input'>Filmi tootja: </label>
+		<input type='text' name='studio_input' id='studio_input' placeholder='tootja'>
+		<br>
+		<label for='director_input'>Filmi lavastaja: </label>
+		<input type='text' name='director_input' id='director_input' placeholder='lavastaja'>
+		<br>
+		<input type='submit' name='film_submit' value='Salvesta'>
+	</form>
+	<p><?php echo $film_store_notice; ?></p>
 	
 
 
---
+++ fnc_film.php
@@ -0,0 +1,59 @@
+<?php
+	$database = "if21_ragnar_va";
+	
+	
+	function read_all_films(){
+		
+		//Loome andmebaasiga ühenduse mysqli /serverhost/kasutajanimi/parool/andmebaas;
+		$conn = new mysqli($GLOBALS['server_host'], $GLOBALS["server_user_name"], $GLOBALS['server_password'], $GLOBALS['database']);
+		//Valmistan ette sql päringu SELECT * FROM tabelinimi;
+		$conn->set_charset('utf8');
+		$stmt = $conn->prepare("SELECT * FROM film");
+		
+		echo $conn->error;
+		//Seon tulemused muutujatega
+		$stmt->bind_result($title_from_db, $year_from_db, $duration_from_db, $genre_from_db, $studio_from_db, $director_from_db);
+		//Täidan käsu
+		$film_html = null;
+		$stmt->execute();
+		
+		//Võtan kirjeid kuni jätkub !
+		while($stmt->fetch()){
+			$film_html .= '<h3>' . $title_from_db . '</h3>';
+			$film_html .= '<ul>';
+			$film_html .= '<li>Valmimis aasta:' . $year_from_db . '</li>';
+			$film_html .= '<li>Kestus:' . $duration_from_db . '</li>';
+			$film_html .= '<li>Zanr:' . $genre_from_db . '</li>';
+			$film_html .= '<li>Tootja::' . $studio_from_db . '</li>';
+			$film_html .= '<li>Lavastaja:' . $director_from_db . '</li>';
+			$film_html .= '</ul>';
+		}
+		//Sulgen käsu
+		$stmt->close();
+		//Sulgeme andmebaasi ühenduse
+		$conn->close();
+		return $film_html;
+	}
+	
+	function store_film($title_input, $year_input, $duration_input, $genre_input, $studio_input, $director_input){
+		$conn = new mysqli($GLOBALS['server_host'], $GLOBALS["server_user_name"], $GLOBALS['server_password'], $GLOBALS['database']);
+		$conn->set_charset('utf8');
+		//SQL: INSERT INTO film (pealkiri, aasta, kestus, zanr, tootja, lavastaja) VALUES('SUVI', 1976, 83, 'Tallinn film', 'Arvo Kruusement')
+		$stmt = $conn->prepare('INSERT INTO film (pealkiri, aasta, kestus, zanr, tootja, lavastaja) VALUES(?,?,?,?,?,?)');
+		echo $conn->error;
+		//seome SQL käsuga päris andmed!
+		//i - integer, d - decimal, s - string
+		$stmt->bind_param('siisss', $title_input, $year_input, $duration_input, $genre_input, $studio_input, $director_input);
+		//käsu täitmine!
+		$success = null;
+		if($stmt->execute()){
+			$success = 'Salvestamine õnnestus';
+		} else {
+			$success = 'Salvestamisel tekkis viga: ' . $stmt->error;
+		}
+		
+		$stmt->close();
+		$conn->close();
+		return $success;
+	}
+?>
